b7eab4fcb81b812c0f8f4eea97ae49f9f8ec5e0e
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2019-09-22 21:54:59 +0300

    Update migrate-and-postgrest.sh

--- backend-res/migrate-and-postgrest.sh
+++ backend-res/migrate-and-postgrest.sh
@@ -2,6 +2,6 @@
 
 sleep 20;
 
-flyway -url=${POSTGRES_URI} -schemas=${PGRST_DB_SCHEMA} -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} -connectRetries=60 migrate
+flyway -url=${POSTGRES_URI} -schemas=${PGRST_DB_SCHEMA} -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} -connectRetries=60 -locations=filesystem:/flyway/sql migrate
 
 postgrest /etc/postgrest.conf
