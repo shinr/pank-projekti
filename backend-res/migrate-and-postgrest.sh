ad74c88ebb1717c79c32ccf768029d0c50febe2e
jlansineva <jukka.lansineva@dnainternet.net>
2019-09-22 21:15:00 +0300

    removed possible loop

--- backend-res/migrate-and-postgrest.sh
+++ backend-res/migrate-and-postgrest.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-while ! nc -z db 5432; do \
-    echo "Waiting... ${POSTGRES_URI}";
-    sleep 0.5;
-done;
+sleep 20;
 
 flyway -url=${POSTGRES_URI} -schemas=${PGRST_DB_SCHEMA} -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} -connectRetries=60 migrate
 
