af44513ac5457d4f09e7d95bcb61ab3d205ea38d
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2019-10-20 17:41:39 +0300

    Update setup-jwt.sh

--- backend-res/setup-jwt.sh
+++ backend-res/setup-jwt.sh
@@ -1,3 +1,6 @@
 # su - "$POSTGRES_USER" -c '/usr/local/bin/pg_ctl -D /var/lib/postgresql/data -o "-c listen_addresses='localhost'" -w start'
+case $CI in
+  (true) PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c "create schema if not exists api" ;;
+esac
 PGPASSWORD=${POSTGRES_PASSWORD} psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c "alter database ${POSTGRES_DB} SET \"app.jwt_secret\" TO \"${JWT_SECRET}\";"
 # su - "$POSTGRES_USER" -c '/usr/local/bin/pg_ctl -D /var/lib/postgresql/data -m fast -w stop'
