f20b5a0b528d172e48415ef50fe6ec97e7b3dd68
Jukka <Jukka Lnsineva>
2019-11-08 21:03:26 +0200

    eventcalender component refreshed

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { timestampToObject } from "../../utils/date"
+
 import styles from "./EventCard.module.css"
 
 export const EventCard = ({ headline, content, date, index }) => {
     const [expanded, expand] = useState(false)
-    const parsedDate = new Date(date)
+    const parsedDate = timestampToObject(new Date(date))
     console.log(index, index % 2 === 0)
     return (
         <div className={styles.eventcard}>
-            <label className={index % 2 === 0 ? styles.even : styles.odd  } onClick={() => expand(!expanded)}>
-                <span><FontAwesomeIcon icon="calendar-alt" /></span>
-                <span>{parsedDate.toDateString()}</span>
-                <span>{headline}</span>
-                <span><FontAwesomeIcon icon="caret-right" /></span>
-            </label>
-            {expanded ? <div className={styles.eventcard__expanded}>{content}</div> : <div></div>}
+            <div className={`${styles.eventcard__content} ${index % 2 === 0 ? styles.even : styles.odd}`} onClick={() => expand(!expanded)}>
+                <div className={styles.eventcard__date}>
+                    <span>{parsedDate.date}</span>
+                    <span>{parsedDate.month}</span>
+                </div>
+                <div className={styles.eventcard__title}>
+                    <div>{headline}</div>
+                    <div>Energia areena</div>
+                </div>
+                <div className={styles.eventcard__icon}>
+                    <FontAwesomeIcon icon={expanded ? "caret-down" : "caret-right"} size="2x" />
+                </div>
+            </div>
+            {expanded ? <div className={styles.eventcard__expanded}>
+                <span>{`${parsedDate.day}na ${parsedDate.date}. ${parsedDate.month}ta ${parsedDate.year}`}</span>
+                {content}
+            </div> : <div></div>}
         </div>
     )
 }
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,29 @@
+const dayToString = ["sunnuntai", "maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai"]
+const monthToString = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"]
+
 export const timestampToDDMMYYYY = (timestamp) => {
     return new Date(timestamp)
+}
+
+export const timestampToObject = timestamp => {
+    return {
+        day: dayToString[timestamp.getDay()],
+        month: monthToString[timestamp.getMonth()],
+        year: timestamp.getFullYear(),
+        date: timestamp.getDate()
+    }
+}
+
+export const shortEventTimestamp = (timestamp) => {
+    const formatted = `${timestamp.getDate()}. `
+        + `${monthToString[timestamp.getMonth()]}ta `
+    return formatted
+}
+
+export const eventTimestamp = (timestamp) => {
+    const formatted = `${dayToString[timestamp.getDay()]} `
+        + `${timestamp.getDate()}. `
+        + `${monthToString[timestamp.getMonth()]}ta `
+        + `${timestamp.getFullYear()}`
+    return formatted;
 }
