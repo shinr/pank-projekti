b471a1a52fb57b3cc650d0d89352baa2afe8f435
Juho Toratti <kiiwi45@gmail.com>
2019-11-14 19:34:15 +0200

    Update Main.js

--- src/views/main/Main.js
+++ src/views/main/Main.js
@@ -8,6 +8,11 @@ export function Main() {
         <section className={styles.general_row}>
             <section className={styles.general_column}>
                 <h1>Tervetuloa PANK ryn uudistetuille kotisivuille!</h1>
+                <a>Jätä palautteesi klikkaamalla alla olevaa painiketta
+                <form action="https://docs.google.com/forms/d/e/1FAIpQLSdwx2K9XKTPbVrL2DRfLZteiAHG6RGT2j6F425fUIXrO4tlRQ/viewform">
+                    <input type="submit" value="Anna Palautetta" />
+                    </form>
+                </a>
             </section>
             <section className={styles.general_column_30}>
                 <EventCalendar />
