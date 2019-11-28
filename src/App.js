9f2db9c0854ecd533684f671a182708837b542c0
Juho Toratti <kiiwi45@gmail.com>
2019-11-28 23:09:55 +0200

    Tweak mobile responsivity
    Add mobile responsivity to navbar and some content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter, Route, Link, Switch } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt, faHome, faNewspaper, faServer, faBook } from '@fortawesome/free-solid-svg-icons'
 
 import { AppStateProvider, initialAppState } from "./state/state"
 import NavBar from './ui/navbar'
@@ -10,23 +10,24 @@ import News from './views/news'
 import Main from './views/main'
 import Organization from './views/organization'
 import Documents from './views/documents'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './App.css';
 import { appReducer } from './state/reducers';
 
 function App() {
   // add fontawesome icons into the library, are used with FontAwesomeIcon icon="" -component
-  library.add(faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt)
+  library.add(faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt, faHome, faNewspaper, faServer, faBook)
   return (
     <AppStateProvider initialState={initialAppState} reducer={appReducer}>
       <HashRouter>
         <div className="App">
           <header className="App-header">
             <NavBar>
-              <Link to="/">Koti</Link>
-              <Link to="/news">Uutiset</Link>
-              <Link to="/pank">Organisaatio</Link>
-              <Link to="/documents">Tietopankki</Link>
+              <Link to="/"><FontAwesomeIcon icon="home"/>Koti</Link>
+              <Link to="/news"><FontAwesomeIcon icon="newspaper"/>Uutiset</Link>
+              <Link to="/pank"><FontAwesomeIcon icon="book"/>Organisaatio</Link>
+              <Link to="/documents"><FontAwesomeIcon icon="server"/>Tietopankki</Link>
             </NavBar>
           </header>
           <main className="App-main">
