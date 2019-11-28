dec239a25ac4d99b38d431743cbf0121ac6ef7b8
Juho Toratti <kiiwi45@gmail.com>
2019-11-29 00:07:41 +0200

    Add responsivity for mobile
    Add responsivity for mobile, navbar and some content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt, faHome, faNewspaper, faServer, faBook } from '@fortawesome/free-solid-svg-icons'
 
 import { AppStateProvider, initialAppState } from "./state/state"
 import { appReducer } from './state/reducers';
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   // add fontawesome icons into the library, are used with FontAwesomeIcon icon="" -component
-  library.add(faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt)
+  library.add(faCheckSquare, faBars, faCaretDown, faCaretRight, faUser, faDownload, faCalendar, faCalendarAlt, faHome, faNewspaper, faServer, faBook)
   return (
     <HashRouter>
       <AppStateProvider initialState={initialAppState} reducer={appReducer}>
