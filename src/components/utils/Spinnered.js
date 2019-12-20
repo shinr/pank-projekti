5d88d0091edd90d404fd4195fbf21dc5f45ab338
JToratti <kiiwi45@gmail.com>
2019-12-20 10:39:41 +0200

    fix spinner affect to tietopankki svg

--- src/components/utils/Spinnered.js
+++ src/components/utils/Spinnered.js
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import styles from "../utils/Spinnered.module.css"
 
 export const Spinnered = ({ fetching, children }) => {
-    return (<div className={styles.spinnered}>{ fetching ?
-        <FontAwesomeIcon icon="spinner" pulse size="3x" />
+    return (<div>{ fetching ?
+        <FontAwesomeIcon className={styles.spinnered} icon="spinner" pulse size="3x" />
     : children }</div>)
 
 }
